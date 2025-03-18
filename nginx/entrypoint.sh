1167d679178a5eea88e11bc2e055c71317400f34
Chris Frewin <chris.frewin@solve.ch>
2025-03-18 18:36:41 +0100

    copy both staging and prod api

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ "$ENVIRONMENT" = "staging" ]; then
-  cp /etc/nginx/conf.d/staging.api.codevideo.io.conf /etc/nginx/conf.d/default.conf
-else
-  cp /etc/nginx/conf.d/api.codevideo.io.conf /etc/nginx/conf.d/default.conf
-fi
+cp /etc/nginx/conf.d/staging.api.codevideo.io.conf /etc/nginx/conf.d/default.conf
+cp /etc/nginx/conf.d/api.codevideo.io.conf /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
